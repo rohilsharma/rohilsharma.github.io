d47e9cc592b8cd02a56af8bb4c3cc49781feb031
Rohil Sharma <rohilsharma97@gmail.com>
2019-05-27 18:54:45 -0400

    Updates

--- precache-manifest.b16eea2c5a5fd7cc8229caefee1d7999.js
+++ precache-manifest.b16eea2c5a5fd7cc8229caefee1d7999.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3f7db6013ae3881aa396a23c35f72bdc",
+    "revision": "87bc81f009b74cb944d7abe209c58f6f",
     "url": "/index.html"
   },
   {
-    "revision": "6e420915db23b4a8f79e",
+    "revision": "e1fee277fa0aaccd97d7",
     "url": "/static/css/main.5facb584.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.82d857d4.chunk.js"
   },
   {
-    "revision": "6e420915db23b4a8f79e",
-    "url": "/static/js/main.45663a18.chunk.js"
+    "revision": "e1fee277fa0aaccd97d7",
+    "url": "/static/js/main.41d39eec.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
