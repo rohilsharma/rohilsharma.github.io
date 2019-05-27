9665e530c6255f410888ec88fb515301dc355bfa
Rohil Sharma <rohilsharma97@gmail.com>
2019-05-26 21:24:14 -0400

    Testing V2?

---
+++ build/precache-manifest.f627dcbeac4e36aedfbad74df01b43c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3f9b21743bdbd5cfcb75a0e927f341",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c669bdc2c41b68924109",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c669bdc2c41b68924109",
+    "url": "/static/js/main.2c841fa5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
