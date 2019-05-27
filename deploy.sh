fd24b799d318feeef126a2f95f217ce23f818936
Rohil Sharma <rohilsharma97@gmail.com>
2019-05-26 21:20:44 -0400

    Renamed deploy.sh to deploy_to_master.sh for clarity. Fixed commit and push issues to development branch

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-npm run deploy
-
-git checkout master
-git reset --hard gh-pages
-git push -f origin master
-
-git checkout development
---
+++ deploy_to_master.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then 
+	echo "Usage: $0 <commit message for development branch>"
+	exit 0
+fi
+
+npm run deploy
+
+git add ./build/*
+git commit -m $1
+git push 
+
+#git checkout master
+#git reset --hard gh-pages
+#git push -f origin master
+
+git checkout development
