2968a81f65801fcbf08c80e72281c0ef36a8042c
Rohil Sharma <rohilsharma97@gmail.com>
2019-05-26 21:28:48 -0400

    V4?

--- deploy_to_master.sh
+++ deploy_to_master.sh
@@ -11,8 +11,12 @@ git add .
 git commit -m $1
 git push 
 
+echo "Pushed development to remote. Resetting master now..."
+
 #git checkout master
 #git reset --hard gh-pages
 #git push -f origin master
+
+echo "master updated."
 
 git checkout development
