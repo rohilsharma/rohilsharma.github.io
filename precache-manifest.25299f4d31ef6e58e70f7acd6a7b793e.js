29a232248dc7447a356023ef7b1f11466fc601b2
Rohil Sharma <rohilsharma97@gmail.com>
2020-03-08 18:13:15 -0700

    Updates

---
+++ precache-manifest.25299f4d31ef6e58e70f7acd6a7b793e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf679b39c0aaeb80e6371a6a5d8cc96b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbe30d00e4af4fdf40cb",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "ce571bc2cca99456e5d0",
+    "url": "/static/js/2.82d857d4.chunk.js"
+  },
+  {
+    "revision": "bbe30d00e4af4fdf40cb",
+    "url": "/static/js/main.272d62d5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
