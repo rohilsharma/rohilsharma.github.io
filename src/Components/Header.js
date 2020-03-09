aced6fc87e323e86114345ab918ea1d45718a5cc
Rohil Sharma <rohilsharma97@gmail.com>
2020-03-08 18:11:16 -0700

    Updated main Welcome page
    > Updated main background image
    > Modified colors on main Welcome page to fit new image
    > Ensured all hyperlinks open in new tab instead of current tab

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,48 +7,48 @@ class Header extends Component {
       var name = this.props.data.name;
       var resumeDownload = this.props.data.resumedownload;
       var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+        return <li key={network.name}><a href={network.url} target="_blank"><i className={network.className}></i></a></li>
       })
     }
 
     return (
       <header id="home">
 
-      <nav id="nav-wrap">
+        <nav id="nav-wrap">
 
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+  	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            {/* <li><a className="smoothscroll" href="#portfolio">Works</a></li> */}
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
-         </ul>
+           <ul id="nav" className="nav">
+              <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+  	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
+              {/* <li><a className="smoothscroll" href="#portfolio">Works</a></li> */}
+              <li><a className="smoothscroll" href="#contact">Contact</a></li>
+           </ul>
 
-      </nav>
+        </nav>
 
-      <div className="row banner">
-         <div className="banner-text">
-            <h2>Hello, I'm </h2>
-            <h1 className="responsive-headline">{name}</h1>
-            <hr />
-            <div className="download">
-                  <p>
-                     <a href={resumeDownload} target="_blank" rel="noopener noreferrer" className="button"><i className="fa fa-download"></i> View Resumé</a>
-                  </p>
-            </div>
-            <ul className="social">
-               {networks}
-            </ul>
-         </div>
-      </div>
+        <div className="row banner">
+           <div className="banner-text">
+              <h2>Hi! I'm</h2>
+              <h1 className="responsive-headline">{name}</h1>
+              <hr />
+              <div className="download">
+                    <p>
+                       <a href={resumeDownload} target="_blank" rel="noopener noreferrer" className="button"><i className="fa fa-download"></i> View Resumé</a>
+                    </p>
+              </div>
+              <ul className="social">
+                 {networks}
+              </ul>
+           </div>
+        </div>
 
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#resume"><i className="icon-down-circle"></i></a>
-      </p>
+        <p className="scrolldown">
+           <a className="smoothscroll" href="#resume"><i className="icon-down-circle"></i></a>
+        </p>
 
-   </header>
+      </header>
     );
   }
 }
